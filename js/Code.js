9e0494f1a520d85a2e60e70fd3ef76cfd2d2a0a0
peteruithoven <peter@peteruithoven.nl>
2014-07-11 11:25:14 +0200

    Parsing to better data model (D3 ready)

--- js/Code.js
+++ js/Code.js
@@ -27,56 +27,83 @@ function Code() {
 	};
 	function parse() {
 		
-		_self.lines = {};
-		_self.length = 0;
+		_self.lines = [];
 		var absoluteTimes = {}; // absolute time per machine
+		
 		// go through all set points
+		// group setPoints based on absolute time
 		for (var i in _setPointSets) {
 			var setPointSet = _setPointSets[i];
 			for (var j in setPointSet) {
 				var setPoint = setPointSet[j];
-				//console.log("  setPoint: ",setPoint);
+				var machineLabel = getMachineLabel(setPoint.A);
+				
+				// convert point and time to ints
+				setPoint.T = Number.parseInt(setPoint.T);
+				setPoint.P = Number.parseInt(setPoint.P);
 				
 				// calculate absolute time (per machine)
-				t = absoluteTimes[setPoint.A];
-				if (t === undefined) { t = 0 };
-				absoluteTimes[setPoint.A] = t += Number.parseInt(setPoint.T);
-				//   collect raw setpints per absolute time
-				var line = _self.lines[t];
+				time = absoluteTimes[machineLabel];
+				if (time === undefined) { time = 0 };
+				absoluteTimes[machineLabel] = time += setPoint.T;
+				
+				// find existing line or add new
+				var line = getLine(time);
 				if(line === undefined) {
-					line = _self.lines[t] = {};
-					_self.length++;
+					line = {time:time,setPoints:{}};
+					_self.lines.push(line);
 				}
-				line[setPoint.A] = setPoint;
+				//   collect raw setpints per absolute time
+				line.setPoints[machineLabel] = setPoint;
 			}
 		}
 		
 		// sort lines based on absolute time
-		_self.lines = sortObj(_self.lines);
+		_self.lines.sort(sortOnTime)
 		
-		for (var timeIndex in _self.lines) {
-			var line = _self.lines[timeIndex];
-			console.log("  "+timeIndex+": line: ",line);
-			_self.timeLength = timeIndex;
+		// parse absolute positions per axis
+		var absolutePos = {}; // absolute position per machine
+		for (var machineLabel in _self.machines) {
+			absolutePos[machineLabel] = 0;
+		}
+		for (var i in _self.lines) {
+			var line = _self.lines[i];			
+			for (var machineLabel in _self.machines) {
+				//absolutePos[machineLabel] = 0;
+				//var machineAddress = _self.machines[machineLabel]
+				var setPoint = line.setPoints[machineLabel];
+				if(setPoint !== undefined) {;
+					absolutePos[machineLabel] += setPoint.P;
+				}
+				line[machineLabel] = absolutePos[machineLabel];
+			}
+		}
+		
+		console.log("lines:");
+		for (var i in _self.lines) {
+			var line = _self.lines[i];
+			console.log("  ",line);
+			_self.timeLength = line.time; // store timeLength
+		}
+
+		_self.length = _self.lines.length;
+	}
+	function sortOnTime(a, b) {
+		if(a.time < b.time) return -1;
+		else if(a.time > b.time) return 1;
+		else 0;
+	}
+	function getLine(time) {
+		for (var i in _self.lines) {
+			var line = _self.lines[i];
+			if(line.time === time) return line;
 		}
 	}
-	function sortObj(obj){
-		var sortedKeys = [];
-		var sortedObj = {};
-
-		// Separate keys and sort them
-		for (var i in obj){
-			sortedKeys.push(i);
+	function getMachineLabel(machineAddress) {
+		for (var machineLabel in _self.machines) {
+			if(_self.machines[machineLabel] === machineAddress) return machineLabel;
 		}
-		sortedKeys.sort();
-
-		// Reconstruct sorted obj based on keys
-		for (var i in sortedKeys){
-			sortedObj[sortedKeys[i]] = obj[sortedKeys[i]];
-		}
-		return sortedObj;
-	}
-
+	}	
 	this.getMachine = function(label) {
 		return _self.machines[label];
 	}
@@ -90,13 +117,23 @@ function Code() {
  */
 
 /* Lines:
- * Object {
- * 	2000: Object {
- * 		101: Object { A: "101", T: "2000", P: "100" },
+ * Array [
+ *  {
+ * 		time: 2000,
+ * 		x: 100,
+ * 		y: 0,
+ * 		setPoints: Object {
+ * 			101: Object { A: "101", T: "2000", P: "100" },
+ * 		},
  * 	},
- * 	4000: Object {
- * 		101: Object { A: "101", T: "2000", P: "-100" },
- * 		102: Object { A: "102", T: "2000", P: "100" },
- * 	},
- * }
+ * 	{
+ * 		time: 4000,
+ * 		x: 0,
+ * 		y: 100,
+ * 		setPoints: Object {
+ * 			101: Object { A: "101", T: "2000", P: "-100" },
+ * 			102: Object { A: "102", T: "2000", P: "100" },
+ * 		}
+ * 	}
+ * ]
  */
